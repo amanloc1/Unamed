5c6fd89a7f96a8bcfe8f3463d0d6347f650818c2
Aman Kumar <48627031+amanloc1@users.noreply.github.com>
2019-12-13 17:02:05 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Booxchange</title>
+	<!--Bootstrap-->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+        <link href="css/bootstrap-4-navbar.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="stylesheet" href="css/index.css" >
+        <link rel="stylesheet" href="css/nav4.css" >
+	</head>
+<body>
+<div>
+<header>
+  <!-- New Navigation Bar5-->
+  <nav >
+      <div class="topnav1">
+          <div>
+            <a class="active" href="#home">Booxchange</a>
+            <a href="#about">About</a>
+            <a href="#contact">Contact</a>
+          </div>
+          <div class="search-container">
+            <form action="/action_page.php">
+              <input type="text" placeholder="Search.." name="search">
+              <button type="submit"><i class="fa fa-search"></i></button>
+            </form>
+          </div>
+        <div class="login-container">
+        <button type="button" a href="#">Register</button>
+        <button onclick="window.location.href = 'others/login/login_index.html';">Login</button>
+        </div>
+      </div>
+  </nav>
+</header>
+<!-- Main Container -->
+  
+  <div class="main container">
+     <div class="about-page">
+        <div class="col-xs-12 col-sm-12"> 
+          <h1>Welcome to <span class="text_color">Booxchange</span></h1>
+            <img src="img/books.jpg">
+            <p style="color: #0b010c;font-size: 22px;margin-top: 20px;"> We believe that a happy customer is a good profit so our business policy is to provide best quality product at our best service</p>
+            <ul>
+              <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">They believed that online retail had diminished the enjoyment of buying books, that it had lost its soul. So they decided to recapture its spirit with an ambitious venture to establish an alternative online bookshop.</a></li>
+              <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">Booxchange is an e-commerce Books and Stationery market place where All books is in one Application.</a></li>
+              <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">Now digital India will become more friendly for kids , students and even for elderly because Booxchange contains every stage of books</a></li>
+              <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">Our main aim at Booxchange is to ensure that top quality books are affordable for everyone. As Such, we carefully select every title in order to bring you the very best in Publishing.</a></li>
+               <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">Booxchange will always be your helping hand which till try best to reach your goal and will provide every best products need to full-fill your necessaries in best price.</a></li>
+               <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">We are one of the nations largest, and fast becoming the nation's favourite, supplier of top quality books. There is no obligation to buy, our prices are good and our service are great.</a></li>
+                <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">Despite our increased size, our passion for what we do remains the same. We want to share our love of reading with the rest of the world.</a></li>
+                <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">We always want to offer the widest possible range of books online, naturally. Currently offering over 10 million different titles, we are constantly on the lookout for new partners so that we can increase that figure.</a></li>
+                <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">We work closely with publishers, suppliers and authors to make sure you’ll always find the book you want when you come to our online store. And as well as giving you all  the choice you could wish for.</a></li>
+                <li style="margin-bottom: 40px;"><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">Until then, we hope you enjoy shopping at www.bookstak.com. It really is a pleasure selling books to you.</a></li>
+            </ul>
+        </div>
+      </div>
+  </div>
+
+<!-- Footer -->
+   <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-6 col-md-3 col-xs-12">
+          <div class="footer-logo">
+            <style>
+                img{
+              max-width: 100%;
+              max-height: 100%;
+              height: 200px;
+              display: block; /* remove extra space below image */
+              }
+            </style>
+            <a href="index.html"><img src="img/logo2.png" alt="fotter logo"></a> 
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-2 col-xs-12 collapsed-block">
+          <div class="footer-links">
+            <h3 class="links-title">Information<a class="expander visible-xs" href="#TabBlock-1">+</a></h3>
+            <div class="tabBlock" id="TabBlock-1">
+              <ul class="list-links list-unstyled">
+                <li><a href="#s">Delivery Information</a></li>
+                <li><a href="#">Discount</a></li>
+                <li><a href="sitemap.html">Sitemap</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="faq.html">FAQs</a></li>
+                <li><a href="#">Terms &amp; Condition</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-3 col-md-2 col-xs-12 collapsed-block">
+          <div class="footer-links">
+            <h3 class="links-title">Insider<a class="expander visible-xs" href="#TabBlock-3">+</a></h3>
+            <div class="tabBlock" id="TabBlock-3">
+              <ul class="list-links list-unstyled">
+                <li><a href="sitemap.html"> Sites Map </a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Trends</a></li>
+                <li><a href="about_us.html">About Us</a></li>
+                <li><a href="contact_us.html">Contact Us</a></li>
+                <li><a href="#">My Orders</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-3 col-md-2 col-xs-12 collapsed-block">
+          <div class="footer-links">
+            <h3 class="links-title">Service<a class="expander visible-xs" href="#TabBlock-4">+</a></h3>
+            <div class="tabBlock" id="TabBlock-4">
+              <ul class="list-links list-unstyled">
+                <li><a href="account_page.html">Account</a></li>
+                <li><a href="wishlist.html">Wishlist</a></li>
+                <li><a href="shopping_cart.html">Shopping Cart</a></li>
+                <li><a href="#">Return Policy</a></li>
+                <li><a href="#">Special</a></li>
+                <li><a href="#">Lookbook</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-3 col-xs-12 collapsed-block">
+          <div class="footer-links">
+            <h3 class="links-title">Working hours<a class="expander visible-xs" href="#TabBlock-5">+</a></h3>
+            <div class="tabBlock" id="TabBlock-5">
+             <!--  <div class="footer-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</div> -->
+              <div class="footer-description"> <b>Monday-Friday:</b> 8.30 a.m. - 5.30 p.m.<br>
+                <b>Saturday:</b> 9.00 a.m. - 2.00 p.m.<br>
+                <b>Sunday:</b> Closed </div>
+              <div class="payment">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-contact">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-4">
+            <div class="footer-contact-item">
+              <div class="footer-contact-icon"> <i class="pe-7s-map-marker"></i> </div>
+              <div class="footer-contact-text"> Hossainpur,Madurdaha Kolkata-700107, West Bangal </div>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="footer-contact-item">
+              <div class="footer-contact-icon"> <i class="pe-7s-call"></i> </div>
+              <div class="footer-contact-text"> + (91) 9163208148, 8116167987 </div>
+            </div>
+          </div>
+          <div class="col-sm-4">
+            <div class="footer-contact-item">
+              <div class="footer-contact-icon"> <i class="pe-7s-mail"></i> </div>
+              <div class="footer-contact-text"> <a href="mailto:amanloc1@gmail.com">amanloc1@gmail.com</a> </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="footer-coppyright">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6 col-xs-12 coppyright"> Copyright © 2017 <a href="#"> Booxchange </a>. All Rights Reserved. </div>
+          <div class="col-sm-6 col-xs-12">
+            <ul class="footer-company-links">
+              <li> <a href="#about-us.php">About-us</a> </li>
+              <li> <a href="#">Careers</a> </li>
+              <li> <a href="#">Privacy Policy</a> </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <a href="#" id="back-to-top" title="Back to top"><i class="fa fa-angle-up"></i></a>
+<!-- End Footer -->
+
+
+<!--Script in order-->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+    crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+    crossorigin="anonymous"></script>
+
+	</body>
+</html>
