01d82774233ee02f74e0d6a4cc767c1853b75529
amanloc1 <48627031+amanloc1@users.noreply.github.com>
2019-12-14 17:10:13 +0530

    color

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Booxchange</title>
+	<title>Boox-I-change</title>
 	<!--for pull-->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
         <link href="css/bootstrap-4-navbar.css" rel="stylesheet">
@@ -19,7 +19,7 @@
   <nav >
       <div class="topnav1">
           <div>
-            <a class="active" href="#home">Booxchange</a>
+            <a class="active" href="#home">Boox-I-change</a>
             <a href="#about">About</a>
             <a href="#contact">Contact</a>
           </div>
@@ -42,17 +42,17 @@
      <div class="about-page">
         <div class="col-xs-12 col-sm-12"> 
           <div class="img">
-          <h1>Welcome to <span class="text_color">Booxchange</span></h1>
+          <h1>Welcome to <span class="text_color">Boox-I-change</span></h1>
           
             <img src="img/books.jpg">
           </div>
             <p style="color: #0b010c;font-size: 22px;margin-top: 20px;"> We believe that a happy customer is a good profit so our business policy is to provide best quality product at our best service</p>
             <ul>
               <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">They believed that online retail had diminished the enjoyment of buying books, that it had lost its soul. So they decided to recapture its spirit with an ambitious venture to establish an alternative online bookshop.</a></li>
-              <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">Booxchange is an e-commerce Books and Stationery market place where All books is in one Application.</a></li>
-              <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">Now digital India will become more friendly for kids , students and even for elderly because Booxchange contains every stage of books</a></li>
-              <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">Our main aim at Booxchange is to ensure that top quality books are affordable for everyone. As Such, we carefully select every title in order to bring you the very best in Publishing.</a></li>
-               <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">Booxchange will always be your helping hand which till try best to reach your goal and will provide every best products need to full-fill your necessaries in best price.</a></li>
+              <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">Boox-I-change is an e-commerce Books and Stationery market place where All books is in one Application.</a></li>
+              <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">Now digital India will become more friendly for kids , students and even for elderly because Boox-I-change contains every stage of books</a></li>
+              <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">Our main aim at Boox-I-change is to ensure that top quality books are affordable for everyone. As Such, we carefully select every title in order to bring you the very best in Publishing.</a></li>
+               <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">Boox-I-change will always be your helping hand which till try best to reach your goal and will provide every best products need to full-fill your necessaries in best price.</a></li>
                <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">We are one of the nations largest, and fast becoming the nation's favourite, supplier of top quality books. There is no obligation to buy, our prices are good and our service are great.</a></li>
                 <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">Despite our increased size, our passion for what we do remains the same. We want to share our love of reading with the rest of the world.</a></li>
                 <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">We always want to offer the widest possible range of books online, naturally. Currently offering over 10 million different titles, we are constantly on the lookout for new partners so that we can increase that figure.</a></li>
@@ -167,7 +167,7 @@
     <div class="footer-coppyright">
       <div class="container">
         <div class="row">
-          <div class="col-sm-6 col-xs-12 coppyright"> Copyright © 2017 <a href="#"> Booxchange </a>. All Rights Reserved. </div>
+          <div class="col-sm-6 col-xs-12 coppyright"> Copyright © 2017 <a href="#"> Boox-I-change </a>. All Rights Reserved. </div>
           <div class="col-sm-6 col-xs-12">
             <ul class="footer-company-links">
               <li> <a href="#about-us.php">About-us</a> </li>
