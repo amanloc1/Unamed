51d6b57ec2ba062dbf39fc17e6644d24b1e5a99b
amanloc1 <48627031+amanloc1@users.noreply.github.com>
2019-12-15 14:51:13 +0530

    sa

--- php/header.php
+++ php/header.php
@@ -1,25 +1,25 @@
 <header>
-<div class="navbar navbar-inverse navbar-fixed-top">
+<nav class="navbar navbar-inverse navbar-fixed-top style="background-color: #e3f2fd;"" >
 	<div class="container">
 		<div class="navbar-header">
-			<a href="index.php" class="navbar-brand">Boox-I-Change</a>
-      <button type="button" class="navbar-toggle" data-target="#myicon" data-toggle="collapse">
-					<span class="icon-bar">About</span>
-			</button>
-      <button type="button" class="navbar-toggle" data-target="#myicon" data-toggle="collapse">
-					<span class="icon-bar">Contact</span>
-			</button>
+        <a href="index.php" class="navbar-brand">Boox-I-Change</a>
+        <button type="button" class="navbar-toggle" data-target="#myicon" data-toggle="collapse">
+            <span class="icon-bar">About</span>
+        </button>
+        <button type="button" class="navbar-toggle" data-target="#myicon" data-toggle="collapse">
+            <span class="icon-bar">Contact</span>
+        </button>
 		</div>
-          <div class="search-container">
+      <div class="search-container">
             <form action="#">
               <input type="text" placeholder="Search.." name="search">
               <button type="submit"><i class="fa fa-search"></i></button>
             </form>
-          </div>
-        <div class="login-container">
+      </div>
+      <div class="login-container">
         <button onclick="window.location.href = 'others/login/login_index.html';">Login</button>
         <button type="button" a href="#">Register</button>
-        </div>
       </div>
-  </nav>
+    </div>
+</nav>
 </header>
