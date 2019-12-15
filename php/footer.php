95fd5a830f99133bd4bd9f267eb74735a51c87d5
amanloc1 <48627031+amanloc1@users.noreply.github.com>
2019-12-15 22:39:37 +0530

    sa

---
+++ php/footer.php
@@ -0,0 +1,155 @@
+<br></br>
+
+<style>
+.footer .fa {
+  padding: 10px;
+  margin: 5px;
+  font-size: 15px;
+  width: 35px;
+  text-align: center;
+  text-decoration: none;
+}
+.fa:hover {
+    opacity: 0.7;
+}
+.fa-facebook {
+  background: #3B5998;
+  color: white;
+}
+
+.fa-twitter {
+  background: #55ACEE;
+  color: white;
+}
+
+.fa-google {
+  background: #dd4b39;
+  color: white;
+}
+
+.fa-linkedin {
+  background: #007bb5;
+  color: white;
+}
+
+.fa-instagram {
+  background: #f5c542;
+  color: white;
+}
+
+.fa-pinterest {
+  background: #cb2027;
+  color: white;
+}
+
+div{
+    color:white;
+}
+ul
+{
+list-style-type: none;
+padding: 0;
+margin: 0;
+text-align: left;
+}
+a{
+    color:white;
+}
+</style>
+
+<div class="footer" style="background-color: #1a1a1a;">
+  <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm">
+                <div class="social">
+                    <ul id="social">
+                    <div>
+                    <a href="#" class="fa fa-facebook"></a>
+                    <a href="#" class="fa fa-twitter"></a>
+                    <a href="#" class="fa fa-google"></a>
+                    <a href="#" class="fa fa-linkedin"></a>
+                    </div>
+                    <div>
+                    <a href="#" class="fa fa-instagram"></a>
+                    <a href="#" class="fa fa-pinterest"></a>
+                    </div>
+                    </ul>
+                </div>
+            </div>
+        <div class="col-sm">
+          <div >
+            <h3 >Information<a href="#"></a></h3>
+            <div >
+              <ul >
+                <li><a href="#">Delivery Information</a></li>
+                <li><a href="#">Discount</a></li>
+                <li><a href="#">Sitemap</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">FAQs</a></li>
+                <li><a href="#">Terms &amp; Condition</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm">
+          <div>
+            <h3>Insider<a  href="#"></a></h3>
+            <div>
+              <ul >
+                <li><a href="#">Sites Map </a></li>
+                <li><a href="#">News</a></li>
+                <li><a href="#">Trends</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">My Orders</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div>
+            <h3>Service<a  href="#-4"></a></h3>
+            <div class="" id="-4">
+              <ul>
+                <li><a href="#">Account</a></li>
+                <li><a href="#">Wishlist</a></li>
+                <li><a href="#">Shopping Cart</a></li>
+                <li><a href="#">Return Policy</a></li>
+                <li><a href="#">Special</a></li>
+                <li><a href="#">Lookbook</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm">
+          <div class="footer-links">
+            <h3>Working hours<a  href="#-5"></a></h3>
+            <div class="" id="-5">
+              <div > <b>Monday-Friday:</b> 8.30 a.m. - 5.30 p.m.<br>
+                <b>Saturday:</b> 9.00 a.m. - 2.00 p.m.<br>
+                <b>Sunday:</b> Closed </div>
+              <div>
+                <ul>
+                  <li><a href="#"><img title="Visa" alt="Visa" src="images/visa.png"></a></li>
+                  <li><a href="#"><img title="Paypal" alt="Paypal" src="images/paypal.png"></a></li>
+                  <li><a href="#"><img title="Discover" alt="Discover" src="images/discover.png"></a></li>
+                  <li><a href="#"><img title="Master Card" alt="Master Card" src="images/master-card.png"></a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+        </div>
+    </div>
+  
+</div>
+
+<div class="copyright" style="background-color: #bfbfbf;">
+      <div class="container">
+        <div class="row">
+          Copyright © 2019 <a href="/myweb/index.php"> Boox-I-change </a>. All Rights Reserved.
+        </div>
+      </div>
+</div>
--- php/index_main.php
+++ php/index_main.php
@@ -8,16 +8,7 @@
           </div>
             <p style="color: #0b010c;font-size: 22px;margin-top: 20px;"> We believe that a happy customer is a good profit so our business policy is to provide best quality product at our best service</p>
             <ul>
-              <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">They believed that online retail had diminished the enjoyment of buying books, that it had lost its soul. So they decided to recapture its spirit with an ambitious venture to establish an alternative online bookshop.</a></li>
-              <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">Boox-I-change is an e-commerce Books and Stationery market place where All books is in one Application.</a></li>
-              <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">Now digital India will become more friendly for kids , students and even for elderly because Boox-I-change contains every stage of books</a></li>
-              <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">Our main aim at Boox-I-change is to ensure that top quality books are affordable for everyone. As Such, we carefully select every title in order to bring you the very best in Publishing.</a></li>
-               <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">Boox-I-change will always be your helping hand which till try best to reach your goal and will provide every best products need to full-fill your necessaries in best price.</a></li>
-               <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">We are one of the nations largest, and fast becoming the nation's favourite, supplier of top quality books. There is no obligation to buy, our prices are good and our service are great.</a></li>
-                <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">Despite our increased size, our passion for what we do remains the same. We want to share our love of reading with the rest of the world.</a></li>
-                <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">We always want to offer the widest possible range of books online, naturally. Currently offering over 10 million different titles, we are constantly on the lookout for new partners so that we can increase that figure.</a></li>
-                <li><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">We work closely with publishers, suppliers and authors to make sure you’ll always find the book you want when you come to our online store. And as well as giving you all  the choice you could wish for.</a></li>
-                <li style="margin-bottom: 40px;"><!-- <i class="fa fa-arrow-right"></i>&nbsp; --> <a href="#">Until then, we hope you enjoy shopping at www.bookstak.com. It really is a pleasure selling books to you.</a></li>
+              Apna Kahani Likho
             </ul>
         </div>
       </div>
