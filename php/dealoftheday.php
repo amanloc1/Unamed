18030dd417b370bb7f1f1a061ff2e70428a954a6
amanloc1 <48627031+amanloc1@users.noreply.github.com>
2019-12-17 01:02:20 +0530

    dealoftheday database needed

--- php/dealoftheday.php
+++ php/dealoftheday.php
@@ -1,5 +1,13 @@
+<style>
+.portlet-title{
+	text-align: left;
+}
+.portlet-body{
+	text-align: center;
+}
+</style>
 <!-- BEGIN CONTENT -->			
-	 <div class="portlet" style="text-align: center;">
+<div class="portlet" >
         <div class="portlet-title">
             <div class="caption">
                 <h2>Deals of the Day</h2>
@@ -11,195 +19,208 @@
 		
 		<div class="portlet-body">
             <div class="owl-carousel owl-carousel6-brands">
-					<div>
-						<div class="product-item">
-						  <div class="pi-img-wrapper">
-							<a href="https://www.bookchor.com/book/9780747591061/Harry-potter-and-the-deathly-hallows-(Adult-Edition)"><img src="https://www.bookchor.com/images/cover/574/9780747591061.jpg" 
-							class="img-responsive center-block hidden-xs" alt="Harry potter a..." style="height: 220px;"></a>
-							 </div>
-						  <h3><center><a href="https://www.bookchor.com/book/9780747591061/Harry-potter-and-the-deathly-hallows-(Adult-Edition)">Harry potter a...</a><center></h3>
-							<div class="pi-price-center">
-								<center>Rs. 371</center>
-							</div>
-							<div class="pi-price-left">
-								<s>Rs. 399</s>
-							</div>
-							<div class="pi-price-right">
-								<s>Rs. 1999</s>
-							</div>
-						</div>
-					  </div>
 					
 					<div>
 						<div class="product-item">
-						  <div class="pi-img-wrapper">
-							<a href="https://www.bookchor.com/book/9781447245223/Philomena"><img src="https://www.bookchor.com/images/cover/574/9781447245223.jpg" 
-							class="img-responsive center-block hidden-xs" alt="Philomena" style="height: 220px;"></a>
+							<div class="pi-img-wrapper">
+								<a href="#">
+									<img src="imgpath" class="img-responsive center-block hidden-xs" alt="bookimg" style="height: 220px;">
+								</a>
 							</div>
-						  <h3><center><a href="https://www.bookchor.com/book/9781447245223/Philomena">Philomena</a><center></h3>
+						  	<h3><a href="product link">name of book</a></h3>
 							<div class="pi-price-center">
-								<center>Rs. 109</center>
+								Rs. our price
 							</div>
 							<div class="pi-price-left">
-								<s>Rs. 129</s>
+								<s>Rs. seller price</s>
 							</div>
 							<div class="pi-price-right">
-								<s>Rs. 399</s>
+								<s>Rs. new price</s>
 							</div>
 						</div>
-					  </div>
-					
-					<div>
-						<div class="product-item">
-						  <div class="pi-img-wrapper">
-							<a href="https://www.bookchor.com/book/9781853260728/Villette"><img src="https://www.bookchor.com/images/cover/574/9781853260728.jpg" 
-							class="img-responsive center-block hidden-xs" alt="Villette" style="height: 220px;"></a>
-							</div>
-						  <h3><center><a href="https://www.bookchor.com/book/9781853260728/Villette">Villette</a><center></h3>
-							<div class="pi-price-center">
-								<center>Rs. 93</center>
-							</div>
-							<div class="pi-price-left">
-								<s>Rs. 118</s>
-							</div>
-							<div class="pi-price-right">
-								<s>Rs. 165</s>
-							</div>
-						</div>
-					  </div>
-					
-					<div>
-						<div class="product-item">
-						  <div class="pi-img-wrapper">
-							<a href="https://www.bookchor.com/book/9780007391585/Sanctus"><img src="https://www.bookchor.com/images/cover/574/9780007391585.jpg" 
-							class="img-responsive center-block hidden-xs" alt="Sanctus" style="height: 220px;"></a>
-							</div>
-						  <h3><center><a href="https://www.bookchor.com/book/9780007391585/Sanctus">Sanctus</a><center></h3>
-							<div class="pi-price-center">
-								<center>Rs. 76</center>
-							</div>
-							<div class="pi-price-left">
-								<s>Rs. 97</s>
-							</div>
-							<div class="pi-price-right">
-								<s>Rs. 806</s>
-							</div>
-						</div>
-					  </div>
-					
-					<div>
-						<div class="product-item">
-						  <div class="pi-img-wrapper">
-							<a href="https://www.bookchor.com/book/9781409137634/Never-Saw-it-Coming"><img src="https://www.bookchor.com/images/cover/574/9781409137634.jpg" 
-							class="img-responsive center-block hidden-xs" alt="Never Saw it C..." style="height: 220px;"></a>
-							</div>
-						  <h3><center><a href="https://www.bookchor.com/book/9781409137634/Never-Saw-it-Coming">Never Saw it C...</a><center></h3>
-							<div class="pi-price-center">
-								<center>Rs. 150</center>
-							</div>
-							<div class="pi-price-left">
-								<s>Rs. 179</s>
-							</div>
-							<div class="pi-price-right">
-								<s>Rs. 399</s>
-							</div>
-						</div>
-					  </div>
-					
-					<div>
-						<div class="product-item">
-						  <div class="pi-img-wrapper">
-							<a href="https://www.bookchor.com/book/9781471127632/Gift-to-Remember"><img src="https://www.bookchor.com/images/cover/574/9781471127632.jpg" 
-							class="img-responsive center-block hidden-xs" alt="Gift to Rememb..." style="height: 220px;"></a>
-							</div>
-						  <h3><center><a href="https://www.bookchor.com/book/9781471127632/Gift-to-Remember">Gift to Rememb...</a><center></h3>
-							<div class="pi-price-center">
-								<center>Rs. 78</center>
-							</div>
-							<div class="pi-price-left">
-								<s>Rs. 108</s>
-							</div>
-							<div class="pi-price-right">
-								<s>Rs. 806</s>
-							</div>
-						</div>
-					  </div>
-					
-					<div>
-						<div class="product-item">
-						  <div class="pi-img-wrapper">
-							<a href="https://www.bookchor.com/book/9780241963579/Childs-Child"><img src="https://www.bookchor.com/images/cover/574/9780241963579.jpg" 
-							class="img-responsive center-block hidden-xs" alt="Childs Child" style="height: 220px;"></a>
-							</div>
-						  <h3><center><a href="https://www.bookchor.com/book/9780241963579/Childs-Child">Childs Child</a><center></h3>
-							<div class="pi-price-center">
-								<center>Rs. 86</center>
-							</div>
-							<div class="pi-price-left">
-								<s>Rs. 108</s>
-							</div>
-							<div class="pi-price-right">
-								<s>Rs. 806</s>
-							</div>
-						</div>
-					  </div>
-					
-					<div>
-						<div class="product-item">
-						  <div class="pi-img-wrapper">
-							<a href="https://www.bookchor.com/book/9780552551328/Girls-under-Pressure"><img src="https://www.bookchor.com/images/cover/574/9780552551328.jpg" 
-							class="img-responsive center-block hidden-xs" alt="Girls under Pr..." style="height: 220px;"></a>
-							</div>
-						  <h3><center><a href="https://www.bookchor.com/book/9780552551328/Girls-under-Pressure">Girls under Pr...</a><center></h3>
-							<div class="pi-price-center">
-								<center>Rs. 82</center>
-							</div>
-							<div class="pi-price-left">
-								<s>Rs. 108</s>
-							</div>
-							<div class="pi-price-right">
-								<s>Rs. 999</s>
-							</div>
-						</div>
-					  </div>
-					
-					<div>
-						<div class="product-item">
-						  <div class="pi-img-wrapper">
-							<a href="https://www.bookchor.com/book/9780755306091/Play-it-Again?"><img src="https://www.bookchor.com/images/cover/574/9780755306091.jpg" 
-							class="img-responsive center-block hidden-xs" alt="Play it Again?" style="height: 220px;"></a>
-							</div>
-						  <h3><center><a href="https://www.bookchor.com/book/9780755306091/Play-it-Again?">Play it Again?</a><center></h3>
-							<div class="pi-price-center">
-								<center>Rs. 88</center>
-							</div>
-							<div class="pi-price-left">
-								<s>Rs. 108</s>
-							</div>
-							<div class="pi-price-right">
-								<s>Rs. 450</s>
-							</div>
-						</div>
-					  </div>
-					
-					<div>
-						<div class="product-item">
-						  <div class="pi-img-wrapper">
-							<a href="https://www.bookchor.com/book/9780143415534/Adventures-in-a-Megacity"><img src="https://www.bookchor.com/images/cover/383/9780143415534.jpg" 
-							class="img-responsive center-block hidden-xs" alt="Adventures in ..." style="height: 220px;"></a>
-							</div>
-						  <h3><center><a href="https://www.bookchor.com/book/9780143415534/Adventures-in-a-Megacity">Adventures in ...</a><center></h3>
-							<div class="pi-price-center">
-								<center>Rs. 111</center>
-							</div>
-							<div class="pi-price-left">
-								<s>Rs. 141</s>
-							</div>
-							<div class="pi-price-right">
-								<s>Rs. 399</s>
-							</div>
-						</div>
-					  </div>
 					</div>
-		</div>	
+					
+					<div>
+						<div class="product-item">
+							<div class="pi-img-wrapper">
+								<a href="#">
+									<img src="imgpath" class="img-responsive center-block hidden-xs" alt="bookimg" style="height: 220px;">
+								</a>
+							</div>
+						  	<h3><a href="product link">name of book</a></h3>
+							<div class="pi-price-center">
+								Rs. our price
+							</div>
+							<div class="pi-price-left">
+								<s>Rs. seller price</s>
+							</div>
+							<div class="pi-price-right">
+								<s>Rs. new price</s>
+							</div>
+						</div>
+					</div>
+					
+					<div>
+						<div class="product-item">
+							<div class="pi-img-wrapper">
+								<a href="#">
+									<img src="imgpath" class="img-responsive center-block hidden-xs" alt="bookimg" style="height: 220px;">
+								</a>
+							</div>
+						  	<h3><a href="product link">name of book</a></h3>
+							<div class="pi-price-center">
+								Rs. our price
+							</div>
+							<div class="pi-price-left">
+								<s>Rs. seller price</s>
+							</div>
+							<div class="pi-price-right">
+								<s>Rs. new price</s>
+							</div>
+						</div>
+					</div>
+					
+					<div>
+						<div class="product-item">
+							<div class="pi-img-wrapper">
+								<a href="#">
+									<img src="imgpath" class="img-responsive center-block hidden-xs" alt="bookimg" style="height: 220px;">
+								</a>
+							</div>
+						  	<h3><a href="product link">name of book</a></h3>
+							<div class="pi-price-center">
+								Rs. our price
+							</div>
+							<div class="pi-price-left">
+								<s>Rs. seller price</s>
+							</div>
+							<div class="pi-price-right">
+								<s>Rs. new price</s>
+							</div>
+						</div>
+					</div>
+					
+					<div>
+						<div class="product-item">
+							<div class="pi-img-wrapper">
+								<a href="#">
+									<img src="imgpath" class="img-responsive center-block hidden-xs" alt="bookimg" style="height: 220px;">
+								</a>
+							</div>
+						  	<h3><a href="product link">name of book</a></h3>
+							<div class="pi-price-center">
+								Rs. our price
+							</div>
+							<div class="pi-price-left">
+								<s>Rs. seller price</s>
+							</div>
+							<div class="pi-price-right">
+								<s>Rs. new price</s>
+							</div>
+						</div>
+					</div>
+					
+					<div>
+						<div class="product-item">
+							<div class="pi-img-wrapper">
+								<a href="#">
+									<img src="imgpath" class="img-responsive center-block hidden-xs" alt="bookimg" style="height: 220px;">
+								</a>
+							</div>
+						  	<h3><a href="product link">name of book</a></h3>
+							<div class="pi-price-center">
+								Rs. our price
+							</div>
+							<div class="pi-price-left">
+								<s>Rs. seller price</s>
+							</div>
+							<div class="pi-price-right">
+								<s>Rs. new price</s>
+							</div>
+						</div>
+					</div>
+					
+					<div>
+						<div class="product-item">
+							<div class="pi-img-wrapper">
+								<a href="#">
+									<img src="imgpath" class="img-responsive center-block hidden-xs" alt="bookimg" style="height: 220px;">
+								</a>
+							</div>
+						  	<h3><a href="product link">name of book</a></h3>
+							<div class="pi-price-center">
+								Rs. our price
+							</div>
+							<div class="pi-price-left">
+								<s>Rs. seller price</s>
+							</div>
+							<div class="pi-price-right">
+								<s>Rs. new price</s>
+							</div>
+						</div>
+					</div>
+					
+					<div>
+						<div class="product-item">
+							<div class="pi-img-wrapper">
+								<a href="#">
+									<img src="imgpath" class="img-responsive center-block hidden-xs" alt="bookimg" style="height: 220px;">
+								</a>
+							</div>
+						  	<h3><a href="product link">name of book</a></h3>
+							<div class="pi-price-center">
+								Rs. our price
+							</div>
+							<div class="pi-price-left">
+								<s>Rs. seller price</s>
+							</div>
+							<div class="pi-price-right">
+								<s>Rs. new price</s>
+							</div>
+						</div>
+					</div>
+					
+					<div>
+						<div class="product-item">
+							<div class="pi-img-wrapper">
+								<a href="#">
+									<img src="imgpath" class="img-responsive center-block hidden-xs" alt="bookimg" style="height: 220px;">
+								</a>
+							</div>
+						  	<h3><a href="product link">name of book</a></h3>
+							<div class="pi-price-center">
+								Rs. our price
+							</div>
+							<div class="pi-price-left">
+								<s>Rs. seller price</s>
+							</div>
+							<div class="pi-price-right">
+								<s>Rs. new price</s>
+							</div>
+						</div>
+					</div>
+					
+					<div>
+						<div class="product-item">
+							<div class="pi-img-wrapper">
+								<a href="#">
+									<img src="imgpath" class="img-responsive center-block hidden-xs" alt="bookimg" style="height: 220px;">
+								</a>
+							</div>
+						  	<h3><a href="product link">name of book</a></h3>
+							<div class="pi-price-center">
+								Rs. our price
+							</div>
+							<div class="pi-price-left">
+								<s>Rs. seller price</s>
+							</div>
+							<div class="pi-price-right">
+								<s>Rs. new price</s>
+							</div>
+						</div>
+					</div>
+					
+			</div>
+		</div>
+</div>
           <!-- END CONTENT -->
