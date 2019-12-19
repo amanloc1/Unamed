66b31e6b9c2a0f04369473a485868f5df0c6fa19
unknown <aman.kumar.cse20@gmail.com>
2019-12-19 11:33:55 +0530

    commit

--- php/dealoftheday.php
+++ php/dealoftheday.php
@@ -20,6 +20,36 @@
 		<div class="portlet-body">
             <div class="owl-carousel owl-carousel6-brands">
 					
+			<?php
+				$conn = mysqli_connect("localhost", "root", "", "book-i-change");
+				// Check connection
+				if ($conn->connect_error) {
+				die("Connection failed: " . $conn->connect_error);
+				}
+				$sql = "SELECT book_name,img_path,our_price,seller_price,new_book_price FROM newlyaddedbooks";
+				$result = $conn->query($sql);
+				if ($result->num_rows > 0)
+				// output data of each row
+				while($row = $result->fetch_assoc()) 
+				{
+			?>
+					<div>
+					<div class="product-item">
+					<div class="pi-img-wrapper">
+					<a href="index.php">
+					<img src="<?php $row["img_path"] ?> " class="img-responsive center-block hidden-xs" alt="bookimg" style="height: 220px;">
+					</a>
+					</div>
+					<h3><a href="index.php"><?php $row["book_name"] ?> </a></h3>
+					<div class="pi-price-center">Rs<?php $row["our_price"] ?></div>
+					<div class="pi-price-left">Rs<?php $row["our_price"] ?> </div>
+					<div class="pi-price-right">Rs<?php $row["our_price"] ?></div>
+					</div>
+				</div>
+			<?php
+				}
+			?>
+
 					<div>
 						<div class="product-item">
 							<div class="pi-img-wrapper">
